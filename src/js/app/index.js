d66a2d1cc9e6da1ba81430e9b086d3b07cca8c96
Olsza <olsza@users.noreply.github.com>
2020-11-03 14:40:33 +0100

    set links from //: to https://git-nauka.czlowiek.it

--- src/js/app/index.js
+++ src/js/app/index.js
@@ -105,10 +105,10 @@ var insertAlternateLinks = function(pageId) {
   // and optionally a region (in ISO 3166-1 Alpha 2 format) of an alternate URL
 
   var altLinks = LocaleStore.getSupportedLocales().map(function(langCode) {
-    var url = "//git-nauka.czlowiek.it/?locale=" + langCode;
+    var url = "https://git-nauka.czlowiek.it/?locale=" + langCode;
     return '<link rel="alternate" hreflang="'+langCode+'" href="' + url +'" />';
   });
-  var defaultUrl = "//git-nauka.czlowiek.it/?locale=" + LocaleStore.getDefaultLocale();
+  var defaultUrl = "https://git-nauka.czlowiek.it/?locale=" + LocaleStore.getDefaultLocale();
   altLinks.push('<link rel="alternate" hreflang="x-default" href="' + defaultUrl +'" />');
   $('head').prepend(altLinks);
 
