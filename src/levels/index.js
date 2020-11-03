b296eb8b2b6467458748b5fc21c1e23b029683a0
Olsza <olsza@users.noreply.github.com>
2020-11-03 13:01:48 +0100

    off levels are not translated

--- src/levels/index.js
+++ src/levels/index.js
@@ -7,48 +7,48 @@ exports.levelSequences = {
     require('./intro/merging').level,
     require('./intro/rebasing').level
   ],
-  rampup: [
-    require('./rampup/detachedHead').level,
-    require('./rampup/relativeRefs').level,
-    require('./rampup/relativeRefs2').level,
-    require('./rampup/reversingChanges').level
-  ],
-  move: [
-    require('./rampup/cherryPick').level,
-    require('./rampup/interactiveRebase').level
-  ],
-  mixed: [
-    require('./mixed/grabbingOneCommit').level,
-    require('./mixed/jugglingCommits').level,
-    require('./mixed/jugglingCommits2').level,
-    require('./mixed/tags').level,
-    require('./mixed/describe').level
-  ],
-  advanced: [
-    require('./rebase/manyRebases').level,
-    require('./advanced/multipleParents').level,
-    require('./rebase/selectiveRebase').level
-  ],
-  remote: [
-    require('./remote/clone').level,
-    require('./remote/remoteBranches').level,
-    require('./remote/fetch').level,
-    require('./remote/pull').level,
-    require('./remote/fakeTeamwork').level,
-    require('./remote/push').level,
-    require('./remote/fetchRebase').level,
-    require('./remote/lockedMaster').level
-  ],
-  remoteAdvanced: [
-    require('./remote/pushManyFeatures').level,
-    require('./remote/mergeManyFeatures').level,
-    require('./remote/tracking').level,
-    require('./remote/pushArgs').level,
-    require('./remote/pushArgs2').level,
-    require('./remote/fetchArgs').level,
-    require('./remote/sourceNothing').level,
-    require('./remote/pullArgs').level
-  ]
+  // rampup: [
+  //   require('./rampup/detachedHead').level,
+  //   require('./rampup/relativeRefs').level,
+  //   require('./rampup/relativeRefs2').level,
+  //   require('./rampup/reversingChanges').level
+  // ],
+  // move: [
+  //   require('./rampup/cherryPick').level,
+  //   require('./rampup/interactiveRebase').level
+  // ],
+  // mixed: [
+  //   require('./mixed/grabbingOneCommit').level,
+  //   require('./mixed/jugglingCommits').level,
+  //   require('./mixed/jugglingCommits2').level,
+  //   require('./mixed/tags').level,
+  //   require('./mixed/describe').level
+  // ],
+  // advanced: [
+  //   require('./rebase/manyRebases').level,
+  //   require('./advanced/multipleParents').level,
+  //   require('./rebase/selectiveRebase').level
+  // ],
+  // remote: [
+  //   require('./remote/clone').level,
+  //   require('./remote/remoteBranches').level,
+  //   require('./remote/fetch').level,
+  //   require('./remote/pull').level,
+  //   require('./remote/fakeTeamwork').level,
+  //   require('./remote/push').level,
+  //   require('./remote/fetchRebase').level,
+  //   require('./remote/lockedMaster').level
+  // ],
+  // remoteAdvanced: [
+  //   require('./remote/pushManyFeatures').level,
+  //   require('./remote/mergeManyFeatures').level,
+  //   require('./remote/tracking').level,
+  //   require('./remote/pushArgs').level,
+  //   require('./remote/pushArgs2').level,
+  //   require('./remote/fetchArgs').level,
+  //   require('./remote/sourceNothing').level,
+  //   require('./remote/pullArgs').level
+  // ]
 };
 
 // there are also cute names and such for sequences
